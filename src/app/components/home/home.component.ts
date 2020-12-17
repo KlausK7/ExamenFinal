1c53830e9a29a631418289f5ae68afa9eaf7b279
KlausK7 <68492613+KlausK7@users.noreply.github.com>
2020-12-17 17:36:26 -0300

    case

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,10 +8,11 @@ import { MatCarousel, MatCarouselComponent } from '@ngmodule/material-carousel';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+    public tittle = 'Electro Hogar Inf3210'
 
   //direcion imagenes
-  slides = [{'image': './assets/electro.jpg'},
-    {'image': './assets/cama.jpg'},
+  slides = [{'image': './assets/d3.jpg'},
+    {'image': './assets/cama2.jpg'},
     {'image': './assets/refri.webp'},
     {'image': './assets/audio.jpg'},
     {'image': './assets/closet.jpg'}
